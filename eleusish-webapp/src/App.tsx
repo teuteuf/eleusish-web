7889a4ce0817c6f9a6f1e363fa77d1a0e343ecc0
TeuTeuf <antoine.teuf@gmail.com>
2020-08-18 23:41:21 +0200

    add a monaco code editor

--- eleusish-webapp/src/App.tsx
+++ eleusish-webapp/src/App.tsx
@@ -1,15 +1,36 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import Editor from '@monaco-editor/react'
 
 const App = (): JSX.Element => {
+  const valueGetter = useRef(() => {
+    // do nothing
+  })
+
+  function handleEditorDidMount(_valueGetter: () => string) {
+    valueGetter.current = _valueGetter
+  }
+
+  function handleShowValue() {
+    alert(valueGetter.current())
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <Editor
+          theme="dark"
+          height="30vh"
+          width="80%"
+          language="javascript"
+          options={{ minimap: { enabled: false } }}
+          editorDidMount={handleEditorDidMount}
+        />
+        <button type="button" onClick={handleShowValue}>
+          code?
+        </button>
         <a
           className="App-link"
           href="https://reactjs.org"
