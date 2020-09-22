ac41067c89abefab7f842586ee84bb2975de791a
TeuTeuf <antoine.teuf@gmail.com>
2020-09-23 00:01:07 +0200

    update rule example, app title, submit button label

--- eleusish-webapp/src/pages/NewRule/NewRulePage.tsx
+++ eleusish-webapp/src/pages/NewRule/NewRulePage.tsx
@@ -5,6 +5,43 @@ import * as RulesService from '../../domain/rules/service'
 import { Rule } from '../../domain/rules/rule'
 
 const defaultRule = `
+const RED = 'red';
+const BLACK = 'black';
+
+const suitesToColor = {
+  Diamonds: RED,
+  Hearts: RED,
+  Clubs: BLACK,
+  Spades: BLACK
+}
+
+const ranksToNumber = {
+  Ace: 1,
+  Two: 2,
+  Three: 3,
+  Four: 4,
+  Five: 5,
+  Six: 6,
+  Seven: 7,
+  Height: 8,
+  Nine: 9,
+  Ten: 10,
+  Jack: 11,
+  Queen: 12,
+  King: 13
+}
+
+function getColor(card) {
+  return suitesToColor[card.Suite]
+}
+
+function getNumber(card) {
+  return ranksToNumber[card.Rank]
+}
+
+// Card example:
+// const card = { Suite: 'Diamonds', Rank: 'Ace' }
+
 function getInitialCards (remainingCards) {
   return [];
 }
@@ -95,7 +132,7 @@ const NewRulePage = (): ReactElement => {
       </div>
       <div className={error && styles.error}>Error: {error ?? 'none'}</div>
       <button disabled={!submitAllowed} type="button" onClick={handleSubmit}>
-        Submit new rule
+        {currentRule != null ? 'Update rule' : 'Submit new rule'}
       </button>
     </div>
   )
